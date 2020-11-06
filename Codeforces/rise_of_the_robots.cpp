909536f884eb87296bfb1058dbb0eeb22252cc7d
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2020-11-06 13:10:42 -0600

    Smallest enclosing circle

--- Codeforces/rise_of_the_robots.cpp
+++ Codeforces/rise_of_the_robots.cpp
@@ -1,5 +1,5 @@
 /*
-    Smalles enclosing circle 
+    Smallest enclosing circle 
     Check thesis: https://codeforces.com/blog/entry/23554
     2.2 A subgradient method
 */
@@ -22,13 +22,10 @@
 #include <utility>
 #include <vector>
 using namespace std;
-
-typedef long double llf;
-
 struct PT {
-    llf x, y;
+    double x, y;
     PT() {}
-    PT(llf x, llf y) : x(x), y(y) {}
+    PT(double x, double y) : x(x), y(y) {}
     PT(const PT &p) : x(p.x), y(p.y) {}
     PT operator+(const PT &p) const { return PT(x + p.x, y + p.y); }
     PT operator-(const PT &p) const { return PT(x - p.x, y - p.y); }
@@ -37,39 +34,32 @@ struct PT {
     bool operator<(const PT &p) const { return x < p.x || (x == p.x && y < p.y); }
 };
 
-llf dot(PT p, PT q) { return p.x * q.x + p.y * q.y; }
-llf dist2(PT p, PT q) { return dot(p - q, p - q); }
-llf dist(PT p, PT q) { return sqrt(dist2(p, q)); }
+double dot(PT p, PT q) { return p.x * q.x + p.y * q.y; }
+double dist2(PT p, PT q) { return dot(p - q, p - q); }
+double dist(PT p, PT q) { return sqrt(dist2(p, q)); }
 
-struct Circle {
-    PT c;
-    llf r;
-    Circle() {}
-    Circle(const PT &c, double r) : c(c), r(r) {}
-};
-
-// O(n * ITER)
-const int ITER = 50000;
-Circle smallestEnclosingCircle(const vector<PT> &C) {
-    PT P(0, 0);
-    llf r, alfa = 0.1;
-    for (auto p : C)
-        P = P + p;
-    P = P / C.size();
+// O(n*ITER)
+const int ITER = 30000;
+PT smallestEnclosingCircle(const vector<PT> &P) {
+    PT C(0, 0);
+    double r2, alfa = 0.1;
+    for (auto p : P)
+        C = C + p;
+    C = C / P.size();
     for (int it = ITER; it; --it) {
         int idx = 0;
-        r = dist2(P, C[0]);
-        for (int i = 1; i < C.size(); ++i) {
-            llf _rr = dist2(P, C[i]);
-            if (r < _rr) {
+        r2 = dist2(C, P[0]);
+        for (int i = 1; i < P.size(); ++i) {
+            double _rr2 = dist2(C, P[i]);
+            if (r2 < _rr2) {
                 idx = i;
-                r = _rr;
+                r2 = _rr2;
             }
         }
-        P = P + (C[idx] - P) * alfa;
+        C = C + (P[idx] - C) * alfa;
         alfa *= 0.999;
     }
-    return Circle(P, r);
+    return C;  // radius = sqrt(r2)
 }
 
 int main() {
@@ -79,19 +69,19 @@ int main() {
     int T;
     for (cin >> T; T; --T) {
         int n;
-        llf R, r;
+        double R, r;
         cin >> n >> R >> r;
-        vector<PT> C(n+1);
-        C[0] = PT(0, 0);
+        vector<PT> P(n+1);
+        P[0] = PT(0, 0);
         for (int i = 1; i <= n; ++i) {
-            cin >> C[i].x >> C[i].y;
-            C[i] = C[i] + C[i-1];
+            cin >> P[i].x >> P[i].y;
+            P[i] = P[i] + P[i-1];
         }
 
-        Circle c = smallestEnclosingCircle(C);
-        c.c = c.c*-1;
+        PT C = smallestEnclosingCircle(P);
+        C = C*-1;
         cout << setprecision(9) << fixed;
-        cout << c.c.x << " " << c.c.y << "\n";
+        cout << C.x << " " << C.y << "\n";
     }
     return 0;
 }
