ece4547cf407e9c8411af5bd4c893961a8770c91
Juan Carlos Sigler Priego <juan@repl.it>
2023-07-22 01:28:50 -0600

    Fix

--- EGOI/2023/find_the_box.cpp
+++ EGOI/2023/find_the_box.cpp
@@ -5,10 +5,11 @@
 typedef std::pair<int, int> pii;
 
 pii query(const std::string& s) {
-    std::cout << s << std::endl;
+    std::cout << "? " << s << std::endl;
     std::cout.flush();
     int r, c;
     std::cin >> r >> c;
+    return {r, c};
 }
 
 pii queryRight(int n, int m) {
@@ -20,8 +21,8 @@ pii queryRight(int n, int m) {
     // (m / 2) (piso) a la derecha
     // luego lo mismo a la izquierda
     for (int i = 0; i < n; ++i) {
-        s += std::string(m / 2, '>');
-        s += std::string(m / 2, '<');
+        s += std::string((m - 1) / 2, '>');
+        s += std::string((m - 1) / 2, '<');
         s += 'v';
     }
     return query(s);
@@ -34,15 +35,15 @@ pii queryLeft(int n, int m) {
     s += std::string(m / 2, '>');
     // Bajamos igual pero ahora de izq a derecha
     for (int i = 0; i < n; ++i) {
-        s += std::string(m / 2, '<');
-        s += std::string(m / 2, '>');
+        s += std::string((m - 1) / 2, '<');
+        s += std::string((m - 1) / 2, '>');
         s += 'v';
     }
     // ahora, es seguro que estamos hasta abajo
     // con un bias
-    // nos movemos (m / 2) + 1 a la izquierda
+    // nos movemos (m + 1) / 2 a la izquierda
     // y todo arriba y así aseguramos terminar abajo de pared
-    s += std::string((m / 2) + 1, '<');
+    s += std::string((m + 1) / 2, '<');
     s += std::string(n, '^');
     return query(s);
 }
@@ -64,11 +65,17 @@ pii solve(int n, int m) {
         // Estamos en (n-1, m/2), nunca chocamos con pared
         // Entonces la respuesta está en la mitad izquierda
         pii q2 = queryLeft(n, m);
-        return {q2.first - 1, q2.second};
+        if (q2.first > 0) {
+            // Está arriba
+            return {q2.first - 1, q2.second};
+        } else {
+            // Estaba en la ultima fila
+            return {n - 1, q2.second - 1};
+        }
     }
     // Tenemos que averiguar si la pared estuvo en la primera fila
-    // o en la columna p1.second + (m / 2)
-    pii q3 = queryL(n, m, q1.second);
+    // o en la columna p1.second + ((m + 1) / 2)
+    pii q3 = queryL(n, m, ((m + 1) / 2) + q1.second);
     if (q3.first < n - 1) {
         // Esta abajo
         return {q3.first + 1, q3.second};
@@ -80,7 +87,7 @@ pii solve(int n, int m) {
 
 int main() {
     int n, m;
-    std::cout << std::string(5, 'c');
+    std::cin >> n >> m;
     pii sol = solve(n, m);
     std::cout << "! " << sol.first << " " << sol.second << std::endl;
     return 0;
