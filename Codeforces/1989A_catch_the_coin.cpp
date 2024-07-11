990596d66e38303fb5e13c7c7877b3302be020d3
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2024-07-10 21:05:41 -0400

    cf 1989A

---
+++ Codeforces/1989A_catch_the_coin.cpp
@@ -0,0 +1,39 @@
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+typedef long long int lld;
+typedef long double llf;
+typedef pair<int, int> pii;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    int x, y;
+    cin >> x >> y;
+    cout << (y < -1 ? "NO" : "YES") << endl;
+  }
+
+  return 0;
+}
